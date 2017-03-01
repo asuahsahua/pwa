1c3feea66aeb2628a742514aeb75253f1d039a4e
Natalie Marion <nmarion@salesforce.com>
2017-03-01 17:57:58 -0500

    Remove discord id from the php file into the yml one

--- app/config/db_parameters.php
+++ app/config/db_parameters.php
@@ -8,6 +8,3 @@ $container->setParameter('database_port', '~');
 $container->setParameter('database_name', trim($db['path'], '/'));
 $container->setParameter('database_user', $db['user']);
 $container->setParameter('database_password', $db['pass']);
-
-$container->setParameter('discord_client_id', getenv('DISCORD_CLIENT_ID'));
-$container->setParameter('discord_client_secret', getenv('DISCORD_CLIENT_SECRET'));
